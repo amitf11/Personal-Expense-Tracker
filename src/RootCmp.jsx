036e38da07372c4b16623516246e4feb100e4adb
Amit <fridamit@gmail.com>
2024-05-11 11:04:52 +0300

    Create basic project template

---
+++ src/RootCmp.jsx
@@ -0,0 +1,18 @@
+import { HomePage } from './pages/HomePage'
+
+function RootCmp() {
+
+  return (
+    <div className='main-container'>
+
+      <main>
+        <HomePage />
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
+
+export default RootCmp
